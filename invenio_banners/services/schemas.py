e9cd3c133b563e1cff76df5e9766b3c99b477e52
Miroslav Bauer <mirekys@gmail.com>
2024-09-19 14:33:38 +0200

    feat(administration): use html editor for message

--- invenio_banners/services/schemas.py
+++ invenio_banners/services/schemas.py
@@ -11,13 +11,13 @@ from datetime import datetime, timezone
 
 from invenio_records_resources.services.records.schema import BaseRecordSchema
 from marshmallow import fields, pre_load
-from marshmallow_utils.fields import TZDateTime
+from marshmallow_utils.fields import SanitizedHTML, TZDateTime
 
 
 class BannerSchema(BaseRecordSchema):
     """Schema for banners."""
 
-    message = fields.String(required=True)
+    message = SanitizedHTML(required=True)
     url_path = fields.String(allow_none=True)
     category = fields.String(required=True, metadata={"default": "info"})
     start_datetime = fields.DateTime(
